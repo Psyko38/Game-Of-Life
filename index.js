89b1606e39daa02cc8dde21d58119cb4a7edb00f
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-02-05 12:01:37 +0100

    add some UI

--- index.js
+++ index.js
@@ -1,94 +1,165 @@
+const canevas = document.getElementById("Gride");
+canevas.width = innerWidth / 1.1;
+canevas.height = innerHeight / 1.5;
+const ctx = canevas.getContext("2d");
+const MatrixHeight = 50;
+const MatrixWidth = 100;
+const boxS = innerHeight / MatrixHeight / 1.5;
+const BoxB = 1;
+const DefaultTime = 2000;
+
+let matrice = [];
+let time = 0;
+let OnOFF = 0;
+
 function createMatrix(rows, cols, initialValue = 0) {
-    let matrix = [];
-    for (let i = 0; i < rows; i++) {
-        let row = [];
-        for (let j = 0; j < cols; j++) {
-            row.push(initialValue);
-        }
-        matrix.push(row);
-    }
-    return matrix;
+	let matrix = [];
+	for (let i = 0; i < rows; i++) {
+		let row = [];
+		for (let j = 0; j < cols; j++) {
+			row.push(initialValue);
+		}
+		matrix.push(row);
+	}
+	return matrix;
 }
 
 function searchMatrix(row, cols, replace) {
-    if (replace === undefined) {
-        return matrice[row][cols];
-    } else {
-        matrice[row][cols] = replace;
-    }
-    return;
+	if (replace === undefined) {
+		return matrice[row][cols];
+	} else {
+		matrice[row][cols] = replace;
+	}
+	return;
 }
 
-let matrice = createMatrix(5, 5, 0);
-console.log(matrice);
-
-searchMatrix(0, 1, 1);
-searchMatrix(1, 2, 1);
-searchMatrix(2, 2, 1);
-searchMatrix(2, 1, 1);
-searchMatrix(2, 0, 1);
-
 function checkGrid3x3(row, col) {
-    let count = 0;
-    let directions = [
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [0, -1],
-        [0, 1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-    ];
-    for (let i = 0; i < directions.length; i++) {
-        let newRow = row + directions[i][0];
-        let newCol = col + directions[i][1];
-        if (
-            newRow >= 0 &&
-            newRow < matrice.length &&
-            newCol >= 0 &&
-            newCol < matrice[0].length &&
-            matrice[newRow][newCol] === 1
-        ) {
-            count++;
-        }
-    }
+	let count = 0;
+	let directions = [
+		[-1, -1],
+		[-1, 0],
+		[-1, 1],
+		[0, -1],
+		[0, 1],
+		[1, -1],
+		[1, 0],
+		[1, 1],
+	];
+	for (let i = 0; i < directions.length; i++) {
+		let newRow = row + directions[i][0];
+		let newCol = col + directions[i][1];
+		if (
+			newRow >= 0 &&
+			newRow < matrice.length &&
+			newCol >= 0 &&
+			newCol < matrice[0].length &&
+			matrice[newRow][newCol] === 1
+		) {
+			count++;
+		}
+	}
 
-    return count;
+	return count;
 }
 
 function cloneMatrix() {
-    let clonedMatrice = createMatrix(matrice.length, matrice[0].length, 0);
-    for (let i = 0; i < matrice.length; i++) {
-        for (let j = 0; j < matrice[i].length; j++) {
-            clonedMatrice[i][j] = matrice[i][j];
-        }
-    }
-    return clonedMatrice;
+	let clonedMatrice = createMatrix(matrice.length, matrice[0].length, 0);
+	for (let i = 0; i < matrice.length; i++) {
+		for (let j = 0; j < matrice[i].length; j++) {
+			clonedMatrice[i][j] = matrice[i][j];
+		}
+	}
+	return clonedMatrice;
 }
 
 function replaceMatrix(Matrice) {
-    matrice = Matrice;
+	matrice = Matrice;
 }
 
 function EditMatrix1(Matrice) {
-    for (let i = 0; i < Matrice.length; i++) {
-        for (let j = 0; j < Matrice[i].length; j++) {
-            let TrueConte = checkGrid3x3(i, j);
-            if (TrueConte === 3) {
-                Matrice[i][j] = 1;
-            }
-            if (TrueConte < 2 || TrueConte > 3) {
-                Matrice[i][j] = 0;
-            }
-        }
-    }
-    return Matrice;
+	for (let i = 0; i < Matrice.length; i++) {
+		for (let j = 0; j < Matrice[i].length; j++) {
+			let TrueConte = checkGrid3x3(i, j);
+			if (TrueConte === 3) {
+				Matrice[i][j] = 1;
+			}
+			if (TrueConte < 2 || TrueConte > 3) {
+				Matrice[i][j] = 0;
+			}
+		}
+	}
+	return Matrice;
 }
 
 function iterate() {
-    let EditMatrix = cloneMatrix();
-    EditMatrix = EditMatrix1(EditMatrix);
-    replaceMatrix(EditMatrix);
-    console.log(matrice);
+	let EditMatrix = cloneMatrix();
+	EditMatrix = EditMatrix1(EditMatrix);
+	replaceMatrix(EditMatrix);
+	CreateCanva(boxS, BoxB, MatrixWidth, MatrixHeight);
 }
+function Setup() {
+	matrice = createMatrix(MatrixHeight, MatrixWidth, 0);
+	CreateCanva(boxS, BoxB, MatrixWidth, MatrixHeight);
+	loop();
+}
+
+function CreateCanva(Box, border, W, H) {
+	ctx.clearRect(0, 0, canevas.width, canevas.height);
+	for (let x = 0; x < W; x++) {
+		for (let y = 0; y < H; y++) {
+			ctx.fillStyle = "rgb(0,255,0)";
+			ctx.fillRect(x * Box, y * Box, Box, Box);
+			if (matrice[y][x] == 1) {
+				ctx.fillStyle = "rgb(0,255,0)";
+			} else {
+				ctx.fillStyle = "rgb(0,50,0)";
+			}
+			ctx.fillRect(
+				x * Box + border / 2,
+				y * Box + border / 2,
+				Box - border,
+				Box - border
+			);
+		}
+	}
+}
+
+const rect = canevas.getBoundingClientRect();
+const canevasX = rect.left;
+const canevasY = rect.top;
+document.addEventListener("click", function (event) {
+	const x = Math.floor((event.clientX - canevasX) / boxS);
+	const y = Math.floor((event.clientY - canevasY) / boxS);
+	if (matrice[y][x] == 1) {
+		matrice[y][x] = 0;
+	} else {
+		matrice[y][x] = 1;
+	}
+	CreateCanva(boxS, BoxB, MatrixWidth, MatrixHeight);
+});
+
+const RunBTN = document.getElementById("run");
+RunBTN.addEventListener("click", function (event) {
+	if (OnOFF == 1) {
+		OnOFF = 0;
+		RunBTN.innerText = "Start";
+	} else {
+		OnOFF = 1;
+		RunBTN.innerText = "Pause";
+	}
+});
+
+const rangeInput = document.getElementById("speed");
+rangeInput.addEventListener("input", function () {
+	const rangeValue = rangeInput.value;
+	time = DefaultTime / rangeValue;
+});
+
+function loop() {
+	if (OnOFF == 1) {
+		iterate();
+	}
+	setTimeout(loop, time);
+}
+
+Setup();
